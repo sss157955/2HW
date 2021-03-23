1a7ed571930d5a3b04e48cbfa24cd2297af24774
MIS803 <MIS803@30803_PC17>
2021-03-23 13:03:53 +0800

    1

--- 2HW/Default.aspx.cs
+++ 2HW/Default.aspx.cs
@@ -8,7 +8,17 @@ using System.Web.UI.WebControls;
 namespace _2HW {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+            int N = 12;
+            int sum = 0;
+            for (int i = 0; i <= N; i++)
+            {
+                if (i % 2 == 0 || i % 3 == 0)
+                {
+                    sum = sum + i;
 
+                }
+            }
+            Response.Write(sum);
         }
     }
 }
